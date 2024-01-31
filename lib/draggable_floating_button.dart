f162482ccb9bb82eb2f63b19a50c19b672219a48
shawn <stardust1900@hotmail.com>
2024-01-31 09:11:29 +0800

    优化刷新按钮

--- lib/draggable_floating_button.dart
+++ lib/draggable_floating_button.dart
@@ -35,7 +35,7 @@ class _DraggableFloatingActionButtonState
     super.initState();
     _offset = widget.initialOffset;
 
-    WidgetsBinding.instance?.addPostFrameCallback(_setBoundary);
+    WidgetsBinding.instance.addPostFrameCallback(_setBoundary);
   }
 
   void _setBoundary(_) {
